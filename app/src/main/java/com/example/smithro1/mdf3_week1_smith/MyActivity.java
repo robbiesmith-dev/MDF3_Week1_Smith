a3288bc42c469f6e603e2542aa287d6cadddfe2c
r2theob23 <r2theob23@gmail.com>
2014-07-09 20:53:19 -0500

    Added go button and url string variable to send user to site entered in edit text, also check to make sure url is in correct format

--- app/src/main/java/com/example/smithro1/mdf3_week1_smith/MyActivity.java
+++ app/src/main/java/com/example/smithro1/mdf3_week1_smith/MyActivity.java
@@ -1,24 +1,32 @@
 package com.example.smithro1.mdf3_week1_smith;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.net.URL;
 
 
 //Robert Smith
 //MDF3 Term 1407
 //Web Browser
-//This will be a Single Activity application that will serve as a Web Browser
+//This will be a Single Activity application that will serve as a Web Browser that can be launched
+//via implicit intent through the OS app chooser
 
 
 public class MyActivity extends Activity {
 
     private WebView mWebView;
     private EditText mEditText;
+    private Button mGoButton;
+    private String mURLString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +35,30 @@ public class MyActivity extends Activity {
 
         //Link member vars with layout counterparts
         mWebView = (WebView)findViewById(R.id.webView);
-        mEditText = (EditText)findViewById(R.id.editText);
-
         mWebView.setWebViewClient(new client());
         mWebView.loadUrl("http://www.google.com");
+
+        mEditText = (EditText)findViewById(R.id.editText);
+        mEditText.setHint("www.google.com");
+
+        mGoButton = (Button)findViewById(R.id.goButton);
+        mGoButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //load url string - first check string format and format it correctly if user if lazy
+                mURLString = mEditText.getText().toString();
+                if(mURLString.startsWith("http://www.")){
+                    mWebView.loadUrl(mURLString);
+                }else if (mURLString.startsWith("www.")){
+                    mWebView.loadUrl("http://" + mURLString);
+                }else{
+                    mWebView.loadUrl("http://www." + mURLString);
+                }
+               
+            }
+        });
+
 
     }
 
